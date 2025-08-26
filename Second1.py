2633ed288fadb4044518bbb8cf2f50616b7364e9
HammadCB <hammadfarooqicb@gmail.com>
2025-08-26 22:32:53 +0500

    Variables and Type changeer + User Input

---
+++ Second1.py
@@ -0,0 +1,16 @@
+# a = "30.1"
+# b = float(a)
+#   = a but the type should be float 
+
+# Shows Class.. of a Data Type 
+# t= type(a) 
+# print(t)
+
+#Learning Input now 
+
+a=  int (input("Enter number 1 "))
+b = int (input("Enter number 2 "))
+
+print("Number A is ",a)
+print("Number B is ",b)
+print("Sum is  ",a+b)
