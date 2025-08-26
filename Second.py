3ab7e14f0510763c02473ac461a78faa6d43c4ee
HammadCB <hammadfarooqicb@gmail.com>
2025-08-26 16:39:26 +0500

    Checked all data types and Operators

---
+++ Second.py
@@ -0,0 +1,47 @@
+a = 3
+b = 5
+c ="Hammad"
+d = True
+e = 3.3
+f = None
+print(a+b+e )
+print(c )
+print( d )
+print(f)
+# Couldnt put different integer type together had to seperate them in different print only int and float worked together 
+
+a=b+e
+print(a)
+
+# Value of a updated but the first print of a did not had any changes codes works as codes work line by line 
+
+h=a<b
+print(h)
+
+#Comparision Operator it says False as the updated a value is 8.3 
+
+b+=3
+print(b)
+
+#Increment in B of 3 
+
+# Logical OperaFalse
+#Truth Table o or 
+
+print("True  or  True is ", True or True )
+print("True  or  False is ", True or False)
+print("False or  True is ", False or True)
+print("False or   False is ", False or False)
+print()
+# Truth Table of AND 
+
+print("True and True is ", True and True )
+print("True and False is ", True and False)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+print()
+# Truth Table of NOT
+
+print("Not True ",  not True)
+print("Not False ", not False)
+print()
