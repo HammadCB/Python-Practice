cc85aba4b5cf154149ecff97a06ffb7dd82cadb4
HammadCB <hammadfarooqicb@gmail.com>
2025-08-27 22:57:01 +0500

    Strings Functions

---
+++ Third_Ch1.py
@@ -0,0 +1,18 @@
+# STRINGS FUNCTIONS 
+
+name = "hammad farooqi"
+#len
+
+print (len(name))
+
+#endswith
+#startswith ( Both are case sesitive )
+ 
+print(name.endswith("aad"))
+print(name.startswith("ham"))
+print(name.startswith("Ham"))
+print(name.capitalize())
+#Capitalize only first not even after space 
+# Escape Sequence 
+a = " \n \"Hammad\" \n is "
+print(a)
