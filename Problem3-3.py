eb0201fdb2a7e80bdce48ef3e0b8b11cd1a791c9
HammadCB <hammadfarooqicb@gmail.com>
2025-08-27 23:18:07 +0500

    Find And Replace

---
+++ Problem3-3.py
@@ -0,0 +1,6 @@
+# Checking Spaces in String or Program
+# Checks where is that specific Input in index value where does it start from
+#  so if we tell it to check space it will give first space and when fixed that it will tell index of next space if there is 
+name = "Hammad is great  guy and "
+
+print(name.find("  "))
