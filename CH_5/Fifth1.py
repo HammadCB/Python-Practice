f8283c2b81558b3c992ac2cba37088bd1d0b5112
HammadCB <hammadfarooqicb@gmail.com>
2025-08-28 19:57:43 +0500

    Sets and Methods

---
+++ CH_5/Fifth1.py
@@ -0,0 +1,30 @@
+# # SETS 
+#  # Empty Dictionary 
+# #     s={}
+# s = {1,5,33,5,33,4}
+# e = set() # Dont use s={} as it will create an empty Dictionary 
+# a =print(type(e))
+# print(s)
+# #In Set it does not Repeat element and it auto sort 
+
+# #Sets METHODS
+
+# t = {1,5,33,5,33,4,"Hammad",6,"Hammad"}
+# t.add(566)
+# print(t)
+
+# print(len(t))
+# t.pop()
+# print(t)
+# t.clear()
+# print(t)
+
+
+
+# Union / Intersection 
+
+s1={1,45,7}
+s2={3,5,1,94}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
