a8b24f259d931908dfd1f49c6cf82c0b39b16335
HammadCB <hammadfarooqicb@gmail.com>
2025-08-28 16:31:01 +0500

    List and List Methods

--- CH_4/Fourth.py
+++ CH_4/Fourth.py
@@ -1,1 +1,42 @@
-print("Hello")
+# # List 
+# # List are mutable Unlike Strings 
+# friends = ["Hammad","Jawad",5,3,3.45,False,"Ali"]
+# print(friends[0])
+# friends[0]="Grapes"
+# print(friends[0])
+
+# #List Slicing 
+# print(friends[1:4])
+
+#----List Methods---- 
+
+Hello = ["Hammad","Jawad",5,3,3.45,False,"Ali"]
+print(Hello)
+
+#Append 
+
+Hello.append("Apple")
+print(Hello)
+
+#Sort / Reverse 
+l1 = [1,23,32,35,15,6]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+
+#Insert 
+
+l2=[1,2,3,4,64,83]
+l2.insert(3,4)
+l2.insert(5,7)
+print(l2)
+
+# #Pop
+
+print(l2.pop(3))
+print(l2)
+
+#Remove Specific input 
+l2.remove(64)
+print(l2)
