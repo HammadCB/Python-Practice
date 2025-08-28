b4082862edbe16f1688eb51e19101325204083df
HammadCB <hammadfarooqicb@gmail.com>
2025-08-28 19:34:45 +0500

    Dictionary and methods

---
+++ CH_5/Fifth.py
@@ -0,0 +1,33 @@
+#Dictionary  
+ # It is unordered , Mutable ,Indexed,No duplicate keyys 
+
+marks = {
+    "Hammad": 100,
+    "Haris": 45,
+    "Sam": 53,
+    0 : "Ahmed "
+}
+
+# print(marks,type(marks))
+# print(marks["Hammad"]) 
+# print(marks[0])
+
+#Dictionary Methods 
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+
+# #Can update Previous written and also add more like this 
+# marks.update({"Hammad":99,"Mike":33})
+# print(marks["Hammad"])
+# print(marks.items())
+
+#Get Method 
+
+# print(marks.get("Ali"))
+print(marks.get("Hammad2"))
+print(marks["Hammad2"])
+# Difference Between them is that .get give return statement if dont found instead of error 
+
+ 
