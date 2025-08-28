eadfe6e50567597f22c0d0d41ed6ce2307d3b6c6
HammadCB <hammadfarooqicb@gmail.com>
2025-08-28 20:37:02 +0500

    Updating Language of Friend name which was inserted Twice

---
+++ CH_5/Problem5_6.py
@@ -0,0 +1,20 @@
+d={}
+name = input("Enter Friends name :")
+lang=input("Enter Language name :")
+d.update({name : lang})
+
+name = input("Enter Friends name :")
+lang=input("Enter Language name :")
+d.update({name : lang})
+
+name = input("Enter Friends name :")
+lang=input("Enter Language name :")
+d.update({name : lang})
+
+name = input("Enter Friends name :")
+lang=input("Enter Language name :")
+d.update({name : lang})
+
+print(d)
+
+#If name is duplicated and Language are different it will take output as Last inserted Language 
