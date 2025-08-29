eae7a4016512d2c21d168bbbd2afecfb9ddfbb60
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 08:15:17 +0500

    Find The Greatest Number

---
+++ CH_6/Problem6_1.py
@@ -0,0 +1,22 @@
+a1 = int(input("Enter number 1 : "))
+a2 = int(input("Enter number 2 : "))
+a3 = int(input("Enter number 3 : "))
+a4 = int(input("Enter number 4 : "))
+
+if(a1>a2 and a1>a3 and a1>a4):
+    print("A1 is the Greatest ")
+
+elif(a2>a1 and a2>a3 and a2>a4):
+    print("A2 is the Greatest ")
+
+elif(a3>a2 and a3>a1 and a3>a4):
+    print("A3 is the Greatest ")
+
+elif(a4>a1 and a4>a2 and a4>a3):
+    print("A4 is the Greatest ")
+
+
+
+
+
+
