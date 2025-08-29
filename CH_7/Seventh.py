02c780f7e27fed68fd65592f54f3c7eac3e001cc
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 09:51:02 +0500

    Loops While

---
+++ CH_7/Seventh.py
@@ -0,0 +1,21 @@
+# Loops 
+for i in range (1,6):
+    print(i)
+
+    #WHILE : Conition must be met falset to get out 
+
+a=1
+while(a<6):
+        print(a)
+        a+=1
+
+ 
+# Print the List Elements 
+
+l=[1,"Hammad",False,"this","ahmed",3]
+
+i=0
+while(i<len(l)):
+    print(l[i])
+    i +=1
+
