e66eac70b1baa3474d43d71a2c8f58260ea1d4bc
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 08:07:20 +0500

    Conditional Expression IF ELIF ELSE

---
+++ CH_6/Sixth.py
@@ -0,0 +1,36 @@
+## CONDITIONAL EXPRESSION 
+
+b = int (input("Enter your age "))
+if(b%2 == 0 ) :
+    print(" B is Even")
+else:
+    print("B is odd")
+
+if(b>=18):
+    print("You are Above the age of consent")
+    print("Good For You")
+elif(b<=0):
+    print("You are entering an invalid age ")
+
+
+else:
+    print("You are below the age of consent")
+
+print("End of Program")
+
+ 
+
+
+# # RELATIONAL OPERATORS 
+
+
+
+
+
+
+
+
+
+
+
+
