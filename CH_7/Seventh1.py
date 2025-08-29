71391dae0af209910a3f252fd81b96758431176c
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 10:11:08 +0500

    For and other Statments

---
+++ CH_7/Seventh1.py
@@ -0,0 +1,86 @@
+# Range function is uded to generate Sequence of numbers 
+
+for i in range(100):
+    print(i)
+
+    
+for p in range(0,100,15):
+    print(p)
+
+
+# Print List in Loop
+
+l=[1,4,6,23,512,32]
+for a in l:
+    print(l)
+
+#Print Tuple in Loop
+
+t=(6,231,75,232,535)
+for x in t:
+    print(x)
+
+# Print Chracter of String in Loop 
+    s="Hammad"
+    for y in s:
+        print(y)
+
+#     -------------------
+# For loop with else 
+
+g=[3,4,6,7,2]
+for item in g:
+    print(item)
+   
+else:
+    print("Done") # This is printed When loop is exhausts
+
+
+
+
+
+#Break Statment 
+for z in range(100):
+    if(z==34):
+        break # Exit Loop
+    print(z)
+
+
+#Continue Statment
+for j in range(100):
+    if(j==34): # Skips 34
+        continue # Continue Rest of Loop
+    print(j)
+
+
+    #Pass Statment 
+
+for k in range(345):
+    pass
+# Pass is A Null Statment it tells to do nothing and just skips 
+k=0
+while(k<45):
+    print(k)
+    k+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
