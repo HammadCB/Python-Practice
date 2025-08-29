40f32675ba37fedd882b6bdfadf209e6b144e097
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 08:25:21 +0500

    Pass/Fail By Percentage

---
+++ CH_6/Problem6_2.py
@@ -0,0 +1,19 @@
+marks1 = int (input("Enter Marks 1 : "))
+marks2 = int (input("Enter Marks 2 : "))
+marks3 = int (input("Enter Marks 3 : "))
+
+total_percentage = ((100)*(marks1+marks2+marks3))/300
+
+if (total_percentage>=40):
+    print("You are pass")
+else:
+    print("You Failed",total_percentage)
+
+
+
+
+
+
+
+
+
