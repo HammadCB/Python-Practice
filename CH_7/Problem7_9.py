d480c8eac81390fc97e2efd6f862cdaccc396819
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 16:55:01 +0500

    Star Pattern 3

---
+++ CH_7/Problem7_9.py
@@ -0,0 +1,31 @@
+''' STAR PATTERN
+ n = 3 
+. . .
+.   .
+. . .
+
+   
+'''
+
+
+
+
+
+n= int(input("Enter the number of lines : "))
+
+for i in range (1,n+1):
+    if(i==1 or i==n):
+        print("*"* n,end="")
+    else:
+        print("*" ,end="")
+        print(" "* (n-2),end="")
+        print("*" ,end="")
+
+    
+
+    print("")   
+
+
+
+
+
