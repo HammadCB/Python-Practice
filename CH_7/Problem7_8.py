b6cc35ddefc1d622201b133c78e0c8f0b5c25aa7
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 15:57:54 +0500

    Start Pattern 2

---
+++ CH_7/Problem7_8.py
@@ -0,0 +1,21 @@
+''' STAR PATTERN
+ n = 4
+.
+..
+...
+....
+
+   
+'''
+
+
+n= int(input("Enter the number of lines : "))
+
+for i in range (1,n+1):# Work till n number of lines 
+    print("*"* (i),end="") #Stars 
+    print("")   
+
+
+
+
+
