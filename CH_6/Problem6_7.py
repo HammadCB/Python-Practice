d5da73d8a510e82154a0117fb2a4158ec9e5b296
HammadCB <hammadfarooqicb@gmail.com>
2025-08-29 08:59:26 +0500

    Name Checker

---
+++ CH_6/Problem6_7.py
@@ -0,0 +1,6 @@
+post = input("Enter the text : ")
+
+if("Hammad".lower() in post.lower()):
+    print("This post is talking about Hammad ")
+else:
+    print("This post is not talking about Hammad")
