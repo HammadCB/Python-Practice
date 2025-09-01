9d084951a3a100f63cb7aab1728061d63166f06c
HammadCB <hammadfarooqicb@gmail.com>
2025-09-01 22:39:08 +0500

    Inheritance

--- CH_OOP_inheritance/inheritance.py
+++ CH_OOP_inheritance/inheritance.py
@@ -1,24 +1,20 @@
 class Employee:
     company="Microsoft"
     def show(self):
-        print(f"The name is {self.name} and salary is {self.salary}")
+        print(f"The name is {self.company} and salary is {self.salary}")
 
 class Programmer:
     company="Google"
     def show(self):
-        print(f"The name is {self.name} and salary is {self.salary}")
-
-    def showlanguage(self):
-        print(f"The name is {self.name} and language is {self.language}")
+        print(f"The name is {self.company} and salary is {self.salary}")
 
     
 class Programmer(Employee): # Inheritance 
         company="Google"
         def show(self):
-            print(f"The name is {self.name} and salary is {self.salary}")
+            print(f"The name is {self.company} and salary is {self.salary}")
 
-        def showlanguage(self):
-            print(f"The name is {self.name} and language is {self.language}")
+
 
 
 a = Employee()
