03dd3afd49ffcca28025621135145ec7c5a98237
HammadCB <hammadfarooqicb@gmail.com>
2025-09-01 19:36:06 +0500

    Class Object

---
+++ CH_OOP_10/Class.py
@@ -0,0 +1,12 @@
+class Employee: # Class Creation
+    language = "Python"# Class attribute
+    salary = 50000
+
+Hammad=Employee() # Object Creation
+Hammad.name="Hammad" # Instance Variable
+print(Hammad.name,Hammad.language,Hammad.salary) # Accessing Class Variable
+
+Ali=Employee() 
+Ali.name="Ali" #Object Attribute
+print(Ali.name,Ali.language,Ali.salary) 
+
