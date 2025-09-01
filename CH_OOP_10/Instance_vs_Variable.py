3d496b0d3ecc08d831d278f6fa1175f3e28a109f
HammadCB <hammadfarooqicb@gmail.com>
2025-09-01 19:40:52 +0500

    Priority of Instance over Class variable

---
+++ CH_OOP_10/Instance_vs_Variable.py
@@ -0,0 +1,15 @@
+class Employee: 
+    language = "Python"
+    salary = 50000
+
+Hammad=Employee() 
+Hammad.language="Java" 
+print(Hammad.language,Hammad.salary) 
+
+Ali=Employee() 
+Ali.language="Java"  
+print(Ali.language,Ali.salary) 
+
+#Instance variable overrides class variable for that instance only 
+
+ 
