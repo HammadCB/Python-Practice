962cf3d16a911b7a82f92d0333fcffcf8bd8a84c
HammadCB <hammadfarooqicb@gmail.com>
2025-09-02 22:48:10 +0500

    Increment Salary

--- CH_OOP_inheritance/Problems/Problem3.py
+++ CH_OOP_inheritance/Problems/Problem3.py
@@ -1,9 +1,26 @@
 class Employee:
-    salary=50000
-    increment=0.5 
+    salary = 50000
+
+    def __init__(self):
+        self._increment = 0.05   # private variable
+
+    @property
+    def increment(self):
+        return self._increment
+
+    @increment.setter
+    def increment(self, salary):
+        # calculate increment % needed to reach target salary
+        self._increment = (salary / self.salary) - 1 
+        
+
     @property
     def salary_after_increment(self):
-        self.salary * self.increment 
+        return self.salary * (1 + self._increment)
 
-e=Employee()
-print(e.salary_after_increment)
+
+e = Employee()
+print(e.salary_after_increment)  # 52500.0 (because 5% increment)
+e.increment = 60000              # set increment so final salary = 60000
+print(e.salary_after_increment)  # 60000.0
+print(e.increment)               # 0.2 (20% increment)
