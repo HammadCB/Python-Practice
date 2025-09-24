e85c0d8bc14af097acabc1e72871a296280c87fa
HammadCB <hammadfarooqicb@gmail.com>
2025-09-24 13:15:01 +0500

    Complext number Addition

---
+++ CH_OOP_inheritance/Problems/Problem4.py
@@ -0,0 +1,16 @@
+class Complex:
+    def __init__(self,r,i):
+        self.r=r
+        self.i=i
+
+
+    def __add__(self,c2):
+        return Complex(self.r+c2.r,self.i+c2.i)
+# So Str function is Converting the output memory object into values 
+# Not the c1 self values 
+    def __str__(self):
+        return f"{self.r}+{self.i}"
+
+c1= Complex(1,3)
+c2= Complex(3,8)
+print(c1 + c2)    
