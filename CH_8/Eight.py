1e37d8dd01a9c722cd3bfa03df3d52a0fd0cb3a9
HammadCB <hammadfarooqicb@gmail.com>
2025-09-24 08:17:13 +0500

    Fixed error

--- CH_8/Eight.py
+++ CH_8/Eight.py
@@ -1,55 +1,55 @@
-# # Functions 
+# Functions 
 
-# a = int(input("Enter your number :"))
-# b = int(input("Enter your number :"))
-# c = int(input("Enter your number :"))
-# average = (a+b+c)/3
-# print(average)
+a = int(input("Enter your number :"))
+b = int(input("Enter your number :"))
+c = int(input("Enter your number :"))
+average = (a+b+c)/3
+print(average)
 
-# # Instead of doing this again and again Create Function
-
-
-# def avg():
-#      a = int(input("Enter your number :"))
-#      b = int(input("Enter your number :"))
-#      c = int(input("Enter your number :"))
-#      average = (a+b+c)/3
-#      print(average)
-
-# avg()
-# avg()
+# Instead of doing this again and again Create Function
 
 
-# #Arguement 
+def avg():
+     a = int(input("Enter your number :"))
+     b = int(input("Enter your number :"))
+     c = int(input("Enter your number :"))
+     average = (a+b+c)/3
+     print(average)
 
-# name = (input("What is you name : "))
-
-# def NAME(name):
-#      print("Good morning : ", name)
-
-# NAME(name)
+avg()
+avg()
 
 
+#Arguement 
 
-# # USER DEFINED FUNCTIONS
-# # BUILT IN FUNCTIONS 
+name = (input("What is you name : "))
 
-# #Arguement
+def NAME(name):
+     print("Good morning : ", name)
 
-# alpha = (input("What is you name : "))
+NAME(name)
 
-# def Bravo(alpha):
-#      print("Good morning : ", alpha)
-#      return  3
-# # RETURN if we want to take value from the function and Put it in Variable out side 
-# a=Bravo(alpha)
-# print(a)
 
-# b=Bravo(alpha)
-# print(b)
 
-# c=a+b
-# print(c)
+# USER DEFINED FUNCTIONS
+# BUILT IN FUNCTIONS 
+
+#Arguement
+
+alpha = (input("What is you name : "))
+
+def Bravo(alpha):
+     print("Good morning : ", alpha)
+     return  3
+# RETURN if we want to take value from the function and Put it in Variable out side 
+a=Bravo(alpha)
+print(a)
+
+b=Bravo(alpha)
+print(b)
+
+c=a+b
+print(c)
 
 
 #If we do not give value we have to give Default Value otherwise Error
